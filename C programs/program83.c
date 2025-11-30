e4855916193ce2f72eea4970bed81c5b2beef116
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2025-11-30 17:46:17 +0530

    Add files via upload

---
+++ C programs/program83.c
@@ -0,0 +1,42 @@
+// Input : 4
+// Output : -4 -3 -2 -1 0 1 2 3 4
+
+
+#include<stdio.h>
+void Display(int iNo)
+{
+    int iCnt = 0;
+    
+    if( iNo < 0)
+    {
+        iNo = - iNo;
+    }
+
+    for(iCnt = -iNo; iCnt <= 0; iCnt++)
+    {
+        printf("%d \t",iCnt);
+    }
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t",iCnt);
+    }
+    printf("\n");
+}
+
+
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the frequency :\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+// Time Complexity : O(N)
+// N is natural number
+// Where N >= 0 and N <= Infinity
