fe9104934d71e79e3979b2d39f504c0d4ad08388
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2025-12-10 09:04:06 +0530

    Add files via upload

---
+++ C programs/program126.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display(int Arr[],int iSize)
+{
+    int iCnt = 0;
+    printf("Entered element");
+    for(iCnt = 0 ; iCnt < iSize; iCnt++)
+    {
+        printf("%d",Arr[iCnt]);
+    }
+}
+int main()
+{
+    int iLength = 0,iCnt =0;;
+    int *ptr = NULL;
+
+    printf("Enter the NUmber of the Element :\n");
+    scanf("%d",&iLength);
+
+    // Step 1: Allocate the memory
+    ptr = (int *)malloc(iLength * sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter the element:\n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+
+   
+    // Step 2: Use the memory
+    Display(ptr,iLength);
+
+    // Step 3: Free the memory
+    free(ptr);
+   
+    
+    return 0;
+}
