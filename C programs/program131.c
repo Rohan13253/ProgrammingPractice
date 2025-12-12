95843e50b205e37057dcc37908cc80894e1dc1e1
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2025-12-12 20:39:49 +0530

    Add files via upload

---
+++ C programs/program131.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int SumOdd(int Arr[],int iSize)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    for(iCnt = 0 ; iCnt < iSize; iCnt++)
+    {
+        if((Arr[iCnt] % 2) != 0)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+        
+    }
+
+    return iSum;
+}
+int main()
+{
+    int iLength = 0,iCnt =0, iRet = 0;
+    int *ptr = NULL;
+
+    printf("Enter the Number of the Element :\n");
+    scanf("%d",&iLength);
+
+    // Step 1: Allocate the memory
+    ptr = (int *)malloc(iLength * sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter the element:\n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+
+   
+    // Step 2: Use the memory
+    iRet = SumOdd(ptr,iLength);
+    printf("Summation of Odd Element are :%d\n",iRet);
+
+    // Step 3: Free the memory
+    free(ptr);
+   
+    
+    return 0;
+}
