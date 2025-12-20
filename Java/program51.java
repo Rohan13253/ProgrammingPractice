94404e6753faa75ae5fd5bf083efbf6e36c1f3d2
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2025-12-20 09:05:13 +0530

    Add files via upload

---
+++ Java/program51.java
@@ -0,0 +1,6 @@
+class program51 
+{
+    public static void main(String A[] ) {
+        System.out.println("Jay ganesh");
+    }
+}
---
+++ Java/program52.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class program52 
+{
+        public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name :");
+        String name = sobj.nextLine();
+
+        System.out.println("Enter your Age:");
+        int Age = sobj.nextInt();
+
+        System.out.println("Enter your Marks :");
+        float Marks = sobj.nextFloat();
+
+        System.out.println("Name " +name);
+        System.out.println("Age " +Age);
+        System.out.println("Marks " +Marks);
+
+    }
+}
