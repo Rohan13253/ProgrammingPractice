5248d2794129855ac6ae436c0f6db874ea549f96
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-01-21 22:56:36 +0530

    Add files via upload

---
+++ C programs/program186.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+// Case inSensentice
+int charFrequency(char *str,char ch)
+{
+    int iCount = 0;
+    
+    while(*str != '\0')
+    {
+        if((*str == ch )|| (*str == ch +32))
+        {
+            iCount++;
+        }
+
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    int iRet = 0;
+    char cValue = '\0';
+    char Arr[50] = {'\0'};
+
+    printf("Enter String :");
+    scanf("%[^'\n']s",Arr);
+
+    
+    printf("Enter Charecter :");
+    scanf(" %c",&cValue);
+
+    iRet = charFrequency(Arr,cValue);
+
+    printf("%c occurs %d times\n",cValue,iRet);
+
+    return 0;
+}
