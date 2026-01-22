00599777f9e8558cc8761ea81f1fe766e7fdc50c
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-01-22 11:18:26 +0530

    Add files via upload

---
+++ C programs/program188.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+// Input  : deRm2dfr
+// Char   : R
+// Output : 2 (Correct)
+
+// Input  : Demo@23@re
+// Char   : @
+// Output : 0 (Wrong)
+
+// Case inSensentice
+
+int charFrequency(char *str,char ch)
+{
+    int iCount = 0;
+
+    
+    
+    while(*str != '\0')
+    {
+        if((ch >= 'a') && (ch <= 'z'))
+        {
+           if(*str == ch || *str == ch - 32)
+            {
+                iCount++;
+            }
+
+        }
+        else if((ch >= 'A') && (ch <= 'Z'))
+        {
+            if(*str == ch || *str == ch + 32)
+            {
+                iCount++;
+            }
+        }
+
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    int iRet = 0;
+    char cValue = '\0';
+    char Arr[50] = {'\0'};
+
+    printf("Enter String :");
+    scanf("%[^'\n']s",Arr);
+
+    
+    printf("Enter Charecter :");
+    scanf(" %c",&cValue);
+
+    iRet = charFrequency(Arr,cValue);
+
+    printf("%c occurs %d times\n",cValue,iRet);
+
+    return 0;
+}
---
+++ C programs/program189.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+// Input  : deRm2dfr
+// Char   : R
+// Output : 2 (Correct)
+
+// Input  : Demo@23@re
+// Char   : @
+// Output : 2 
+
+// Case inSensentice
+
+int charFrequency(char *str,char ch)
+{
+    int iCount = 0;
+
+    
+    
+    while(*str != '\0')
+    {
+        if((ch >= 'a') && (ch <= 'z'))          // Small
+        {
+           if(*str == ch || *str == ch - 32)
+            {
+                iCount++;
+            }
+
+        }
+        else if((ch >= 'A') && (ch <= 'Z'))     // Capital
+        {
+            if(*str == ch || *str == ch + 32)
+            {
+                iCount++;
+            }
+        }
+        else                                    // Other(special char, digits and etc)
+        {
+            if(*str ==ch)
+            {
+                iCount++;
+            }
+        }
+
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    int iRet = 0;
+    char cValue = '\0';
+    char Arr[50] = {'\0'};
+
+    printf("Enter String :");
+    scanf("%[^'\n']s",Arr);
+
+    
+    printf("Enter Charecter :");
+    scanf(" %c",&cValue);
+
+    iRet = charFrequency(Arr,cValue);
+
+    printf("%c occurs %d times\n",cValue,iRet);
+
+    return 0;
+}
