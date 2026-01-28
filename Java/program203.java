58b636ad208972e9cd00052ea226478f3c348ed2
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-01-28 20:55:51 +0530

    Add files via upload

---
+++ Java/program203.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class program203
+{
+    public static void main(String A[])
+    {
+        Scanner Sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name= Sobj.nextLine();
+
+        System.out.println("Your name is :"+name);
+    }
+}
---
+++ Java/program204.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class program204
+{
+    public static void main(String A[])
+    {
+        Scanner Sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name= Sobj.nextLine();
+
+        System.out.println("Your name is :"+name);
+
+        System.out.println("Length :"+name.length());
+    }
+}
