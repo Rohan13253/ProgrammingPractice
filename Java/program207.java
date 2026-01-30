2b799cb01b1b3ffcbee4ad602e57b90c9174869f
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-01-30 23:58:32 +0530

    Add files via upload

---
+++ Java/program207.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class program207
+{
+    public static void main(String A[])
+    {
+        Scanner Sobj = new Scanner(System.in);
+        int iCnt = 0;
+
+        System.out.println("Enter String : ");
+        String name= Sobj.nextLine();
+
+        char Arr[] = name.toCharArray();
+
+        System.out.println(Arr);
+        
+        System.out.println(Arr.length);
+    
+    }
+} 
+    
