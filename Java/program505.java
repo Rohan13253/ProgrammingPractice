74f01cdb23be66821012e1caf5a4576f20ff16fa
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-02-24 22:07:48 +0530

    Add files via upload

---
+++ Java/program505.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class program505
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter The  String : ");
+        String str = sobj.nextLine();
+
+       str = str.replaceAll(" ", "");
+
+       HashMap <Character, Integer>hobj = new HashMap<Character,Integer>();
+
+       char Arr[] = str.toCharArray();
+       int Frequency = 0;
+
+       for(char ch : Arr)
+       {
+            if(hobj.containsKey(ch)) // Character is already there in hashmap
+            {
+                Frequency = hobj.get(ch);
+                hobj.put(ch,Frequency+1);
+            }
+            else // Character occured first time
+            {
+                hobj.put(ch,1);
+            }
+
+            
+       }
+        System.out.println("Unique Character are :");
+        for(char ch : hobj.keySet())
+        {
+            System.out.println(ch);
+        }
+         
+    }    
+}
