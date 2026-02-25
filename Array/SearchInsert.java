138da7fd8afa218d00d3c7fcd185cc1aea30e730
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-02-25 22:58:30 +0530

    Update SearchInsert.java

--- Array/SearchInsert.java
+++ Array/SearchInsert.java
@@ -1,4 +1,13 @@
 
+// 35. Search Insert Position
+// Easy
+
+// Topics
+// premium lock icon
+// Companies
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
 class ArrayX
 {
     int SearchInsertX(int[]arr, int iData)
