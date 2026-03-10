93d22b4d19b21734f9205ae9d6653f5575386923
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-03-10 14:29:27 +0530

    Refactor comments in SearchInsert.java\
    Updated comments for clarity and removed unnecessary lines.

--- Array/SearchInsert.java
+++ Array/SearchInsert.java
@@ -2,12 +2,15 @@
 // 35. Search Insert Position
 // Easy
 
-// Topics
-// premium lock icon
-// Companies
-// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-
+// Given a sorted array of distinct integers and a target value,
+// return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
 // You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2.
+
 class ArrayX
 {
     int SearchInsertX(int[]arr, int iData)
