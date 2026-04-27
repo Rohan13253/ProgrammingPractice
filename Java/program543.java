154a6d6a0e4b8833874161703de91a08337c1a2f
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-04-27 21:34:59 +0530

    Add files via upload

---
+++ Java/program543.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class StudyLog
+{
+    public String subject;
+    public double duraton;
+    public String descrption;
+
+    public StudyLog(String A ,double B ,String C)
+    {
+        this.subject = A;
+        this.duraton = B;
+        this.descrption = C;
+    }
+
+    public void Display()
+    {
+        System.out.println(subject +"|"+duraton+" | "+descrption);
+    }
+}
+
+public class program543 
+{ 
+    public static void main(String[] args) 
+    {
+        StudyLog sobj1 = new StudyLog("C",2.5 , "pointer Array");
+        StudyLog sobj2 = new StudyLog("C++",2 , "pointer Array");
+        StudyLog sobj3 = new StudyLog("Java",3.5 , "Array");
+
+        sobj1.Display();
+        sobj2.Display();
+        sobj3.Display();
+        
+    }   
+}
