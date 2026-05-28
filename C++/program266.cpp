b423480e2acaccab9e72e9b5dd407e075f9775fc
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-05-28 22:12:24 +0530

    Add files via upload

---
+++ C++/program266.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int CountOne(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+
+    while(iNo != 0) 
+    {
+        iDigit = iNo % 2;
+        iNo = iNo / 2;
+        iCnt = iCnt + iDigit;
+    }
+    cout<<endl;
+    return iCnt;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter The number :\n";
+    cin>>iValue;
+
+    iRet = CountOne(iValue);
+    cout<<"Number of ones in binay is :\n";c
+    cout<<iRet;
+    return 0;
+}
