5e7870b73d65ff3861b2d801f5dd1a3c0a8328a9
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-06-04 12:52:41 +0530

    Add files via upload

---
+++ C++/program280.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std ;
+
+typedef unsigned int UINT;
+
+UINT ChangeBit(int iNo)
+{
+    UINT iMask = 4294967231;
+    UINT iResult = 0;
+
+    iResult = iNo & iMask;
+
+    return iResult;
+    
+}
+
+int main()
+{
+    
+    UINT iValue = 0;
+   
+    int iRet = 0;
+
+    cout<<"Enter the number :\n";
+    cin>>iValue;
+
+    iRet = ChangeBit(iValue);
+
+    cout<<"Undated number is :"<<iRet<<"\n";
+   
+
+    return 0;
+}
