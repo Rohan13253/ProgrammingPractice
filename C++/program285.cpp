6632c2d23cec21dc3aa1d0644198b65d435bbe46
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-06-10 20:53:47 +0530

    Add files via upload

---
+++ C++/program285.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std ;
+
+typedef unsigned int UINT;
+
+UINT OffBitMultiple(int iNo) // 4th and 7th
+{
+    UINT iMask1 = 1;
+    UINT iMask2 = 1;
+    UINT iResult = 0;
+
+    iMask1 = iMask1 << 3;
+    iMask2 = iMask2 << 6;
+
+    iMask1 = ~iMask1;
+    iMask2 = ~iMask2;
+
+
+    iResult = iNo & (iMask1 & iMask2);
+    
+    
+   return iResult;
+    
+
+}
+
+int main()
+{
+    
+    UINT iValue = 0;
+    
+    UINT iRet = 0;
+
+    cout<<"Enter the number :\n";
+    cin>>iValue;
+
+    iRet = OffBitMultiple(iValue);
+
+    cout<<"Updated number is :"<<iRet<<"\n";
+
+    return 0;
+}
