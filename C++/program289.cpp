8894025a6f9dd4551f7a5cc23a55c4d8f680430e
Rohan Pawar <142420754+Rohan13253@users.noreply.github.com>
2026-06-14 13:43:22 +0530

    Add files via upload

---
+++ C++/program289.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std ;
+
+bool CheckBit(int iNo)
+{
+    unsigned int iMask = 0x04000000;  // 27th Position 
+    unsigned int iResult = 0;
+
+    iResult = iNo & iMask;
+    
+   return (iResult == iMask);
+    
+
+}
+
+int main()
+{
+    
+    unsigned int iValue = 0;
+    bool bRet = 0;
+
+    cout<<"Enter the number :\n";
+    cin>>iValue;
+
+    bRet = CheckBit(iValue);
+
+    if(bRet == true)
+    {
+        cout<<"27th Bit is ON\n";
+    }
+    else
+    {
+        cout<<"27th Bit is OFF\n";
+    }
+    
+
+    return 0;
+}
